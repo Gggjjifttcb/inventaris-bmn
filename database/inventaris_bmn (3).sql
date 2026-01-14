f0d9d667c3cdbced53bc3de58c090a5f843583cc
unknown <myogaapriadi43@gmail.com>
2026-01-14 15:14:21 +0800

    menambahkan file database sql

--- database/inventaris_bmn (3).sql
+++ database/inventaris_bmn (3).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 09, 2026 at 12:16 AM
+-- Generation Time: Jan 14, 2026 at 07:13 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -30,10 +30,11 @@ SET time_zone = "+00:00";
 CREATE TABLE `inventaris` (
   `id` int NOT NULL,
   `kode` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `nama` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `tahun` int DEFAULT NULL,
+  `nama` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `rak` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `baris` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `box` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `baris` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `image` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `ruang_id` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -42,19 +43,19 @@ CREATE TABLE `inventaris` (
 -- Dumping data for table `inventaris`
 --
 
-INSERT INTO `inventaris` (`id`, `kode`, `nama`, `rak`, `baris`, `box`, `image`, `ruang_id`) VALUES
-(5, 'BMN-213', 'Dokumen keaungan', '1', '1', '1', '1767699531_Dispplay Welcome.jpg', 0),
-(10, '11', 'saya', '2', '2', 'hitam', '1767699371_Dispplay Welcome.jpg', NULL),
-(15, '1j', 'Tas Mewah uya kuya dan istri', '1', '3', '3', '1767757231_command git.png', 10),
-(16, '113', 'kamera', '2', '7', '1', '1767757901_Screenshot 2026-01-04 221333.png', 10),
-(17, '3', 'obeng', '1', '2', '3', '1767767823_Screenshot 2025-12-24 093816.png', 10),
-(18, '4', 'kabel LAN', '1', '2', '3', '1767767846_Screenshot 2025-12-28 201415.png', 10),
-(19, '5', 'acces point zte', '1', '2', '3', '1767767881_Screenshot 2025-12-29 133434.png', 10),
-(20, '6', 'router mikrotik', '1', '2', '3', '1767767901_Screenshot 2025-12-29 133441.png', 10),
-(21, '7', 'mouse', '1', '2', '3', '1767767964_command git.png', 10),
-(22, '8', 'keyborad', '1', '2', '3', '1767767987_command git.png', 10),
-(23, '9', 'komputer', '1', '2', '3', '1767768202_Screenshot 2026-01-07 105033.png', 10),
-(24, '10', 'kamera', '1', '2', '3', '1767771962_command git.png', 10);
+INSERT INTO `inventaris` (`id`, `kode`, `tahun`, `nama`, `rak`, `box`, `baris`, `image`, `ruang_id`) VALUES
+(32, 'dadad', 2016, 'saya', 'fafa1', 'fafa', 'afa1222', '', 10),
+(33, 'ju000', 2016, 'saya', 'saya', 'aku', 'saya', '', 10),
+(34, 'memberikan', 2017, 'kamera', '1', '1', '1', '1767961763_Screenshot 2025-12-24 093816.png', 11),
+(35, 'memberikan', 2016, 'Menerapkan Prosedur  	Menerapkan Prosedur Kesehatan, Keselamatan dan Keamanan Kerja (K3', '11', '1', '1', '1768132181_command git.png', 10),
+(36, 'memberikan', 2018, 'kamera', '1', '2', '2', NULL, 12),
+(37, '11', 2016, 'humas', '1', '1', '12', '1768132205_Screenshot 2025-12-28 201415.png', 10),
+(38, 'yoga1', 2016, 'muhammad yoga1', '111', '1271', '131', '', 10),
+(39, '11', 2017, 'humas', '1', '3', '2', '', 11),
+(40, 'saya', 2017, 'azril', '1', '2', '3', '1768184429_command git.png', 11),
+(41, 'hk.000.11', 2016, 'kegiatan renang', '1', '2', '3', '1768195488_Screenshot 2025-12-24 093816.png', 10),
+(42, '1', 2016, '123', '4', '5', '6', NULL, 10),
+(43, 'kl20', 2016, 'komputer 112', '1', '2', '3', NULL, 10);
 
 -- --------------------------------------------------------
 
@@ -83,7 +84,8 @@ INSERT INTO `ruang` (`id`, `nama_ruang`, `keterangan`) VALUES
 (17, '2023', 'Ruang Arsip'),
 (18, '2024', 'Ruang Arsip'),
 (19, '2025', 'Ruang Arsip'),
-(20, '2026', 'Ruang Arsip');
+(20, '2026', 'Ruang Arsip'),
+(21, '2027', 'Gedung Rektorat');
 
 -- --------------------------------------------------------
 
@@ -134,13 +136,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `inventaris`
 --
 ALTER TABLE `inventaris`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT for table `ruang`
 --
 ALTER TABLE `ruang`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `users`
